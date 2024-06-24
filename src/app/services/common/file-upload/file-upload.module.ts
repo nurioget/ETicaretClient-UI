c909c18ea0bd6fc7050ce0c62013834d60fb90e9
nurioget <nurioget9@gmail.com>
2024-06-24 14:32:16 +0300

    Improving File Upload Mechanism Part1

---
+++ src/app/services/common/file-upload/file-upload.module.ts
@@ -0,0 +1,18 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FileUploadComponent } from './file-upload.component';
+import { NgxFileDropModule } from 'ngx-file-drop';
+
+
+
+@NgModule({
+  declarations: [
+    FileUploadComponent
+  ],
+  imports: [
+    CommonModule,
+    NgxFileDropModule
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+})
+export class FileUploadModule { }
