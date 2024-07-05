2cbcc878f9eef4459d6689ee740d35984b34b0d1
nurioget <nurioget9@gmail.com>
2024-07-05 10:38:29 +0300

    User Registration Form Design (Reactive Form)

--- src/app/ui/components/products/products.component.ts
+++ src/app/ui/components/products/products.component.ts
@@ -15,6 +15,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom)
+    // this.showSpinner(SpinnerType.BallAtom)
   }
 }
