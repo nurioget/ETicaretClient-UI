df52acd0c0f4d2c8a29ca1a3765b73cdbcadb2b8
nurioget <nurioget9@gmail.com>
2024-07-20 14:37:18 +0300

    Update User Password with Reset Password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,9 +9,9 @@ const routes: Routes = [
   {
     path: "admin", component: LayoutComponent, children: [
       { path: "", component: DashboardComponent },
-      { path: "customers", loadChildren: () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule), canActivate: [AuthGuard]},
-      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule), canActivate: [AuthGuard]},
-      { path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule), canActivate: [AuthGuard]}
+      { path: "customers", loadChildren: () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule), canActivate: [AuthGuard] },
+      { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule), canActivate: [AuthGuard] },
+      { path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule), canActivate: [AuthGuard] }
     ], canActivate: [AuthGuard]
   },
   { path: "", component: HomeComponent },
@@ -20,6 +20,8 @@ const routes: Routes = [
   { path: "products/:pageNo", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
   { path: "register", loadChildren: () => import("./ui/components/register/register.module").then(module => module.RegisterModule) },
   { path: "login", loadChildren: () => import("./ui/components/login/login.module").then(module => module.LoginModule) },
+  { path: "password-reset", loadChildren: () => import("./ui/components/password-reset/password-reset.module").then(module => module.PasswordResetModule) },
+  { path: "update-password/:userId/:resetToken", loadChildren: () => import("./ui/components/update-password/update-password.module").then(module => module.UpdatePasswordModule) },
 
 ];
 
--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -7,6 +7,10 @@ import { RegisterComponent } from './register/register.component';
 import { RegisterModule } from './register/register.module';
 import { LoginComponent } from './login/login.component';
 import { LoginModule } from './login/login.module';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
+import { PasswordResetModule } from './password-reset/password-reset.module';
+import { UpdatePasswordModule } from './update-password/update-password.module';
 
 
 
@@ -19,7 +23,9 @@ import { LoginModule } from './login/login.module';
     HomeModule,
     BasketsModule,
     RegisterModule,
-    //LoginModule
+    //LoginModule,
+    PasswordResetModule,
+    UpdatePasswordModule,
   ],
   exports: [
     BasketsModule
